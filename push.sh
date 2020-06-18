5eda76ff23aae72d12dd8738d1de83276d928f45
Rohan Hasabe <43720061+Hasaber8@users.noreply.github.com>
2020-06-18 17:36:18 +0530

    Sign the commits

--- push.sh
+++ push.sh
@@ -14,7 +14,7 @@ cd /public
 git add .
 
 # Commit changes.
-git commit -m "[HasabeCI]: Push Built Site"
+git commit -m "[HasabeCI]: Push Built Site" -s
 
 # Push source and build repos.
 git push origin master
